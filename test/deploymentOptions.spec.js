302430bc10a15e5d6c107eb1636c3a0ea27e5427
Jakub Zakrzewski <Jakub.Zakrzewski@scheer-group.com>
2018-06-18 10:01:48 +0200

    refactor: move deployment options definition to the e2e-bridge-lib

--- test/deploymentOptions.spec.js
+++ test/deploymentOptions.spec.js
@@ -1,35 +1,36 @@
 
 const lib = require('../lib/lib');
+const bridge = require('e2e-bridge-lib');
 
 describe("Deployment options", function() {
 
     it("returns default for empty input", function() {
-        expect(lib.gatherDeploymentOptions()).toEqual({ options: lib.defaultDeploymentOptions });
+        expect(lib.gatherDeploymentOptions()).toEqual({ options: bridge.defaultDeploymentOptions });
     });
 
     it("understands 'startup' option", function() {
-        expect(lib.gatherDeploymentOptions([lib.deploymentOptions.STARTUP]).options.startup).toEqual(true);
+        expect(lib.gatherDeploymentOptions([bridge.deploymentOptions.STARTUP]).options.startup).toEqual(true);
     });
 
     it("understands 'overwrite' option", function() {
-        expect(lib.gatherDeploymentOptions([lib.deploymentOptions.OVERWRITE]).options.overwrite).toEqual(true);
+        expect(lib.gatherDeploymentOptions([bridge.deploymentOptions.OVERWRITE]).options.overwrite).toEqual(true);
     });
 
     it("understands 'overwritePrefs' option", function() {
-        expect(lib.gatherDeploymentOptions([lib.deploymentOptions.SETTINGS]).options.overwritePrefs).toEqual(true);
+        expect(lib.gatherDeploymentOptions([bridge.deploymentOptions.SETTINGS]).options.overwritePrefs).toEqual(true);
     });
 
     it("understands 'runScripts' option", function() {
-        const options = lib.gatherDeploymentOptions([lib.deploymentOptions.NPM_SCRIPTS]).options;
+        const options = lib.gatherDeploymentOptions([bridge.deploymentOptions.NPM_SCRIPTS]).options;
         expect(options.runScripts).toEqual(true);
         expect(options.npmInstall).toEqual(true);
     });
 
     it("understands 'npmInstall' option", function() {
-        expect(lib.gatherDeploymentOptions([lib.deploymentOptions.NPM_INSTALL]).options.npmInstall).toEqual(true);
+        expect(lib.gatherDeploymentOptions([bridge.deploymentOptions.NPM_INSTALL]).options.npmInstall).toEqual(true);
     });
 
     it("understands 'instanceName' option", function() {
-        expect(lib.gatherDeploymentOptions([lib.deploymentOptions.INSTANCE_NAME+'=gugus']).options.instanceName).toEqual('gugus');
+        expect(lib.gatherDeploymentOptions([bridge.deploymentOptions.INSTANCE_NAME+'=gugus']).options.instanceName).toEqual('gugus');
     });
 });
